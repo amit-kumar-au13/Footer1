21ded7e133b4fb49e83b32054351f7f7ece60888
amitkumarau13 <43922503+amitkumarau13@users.noreply.github.com>
2020-10-16 21:03:07 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faYou } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,10 +12,10 @@ function App() {
         <div className = "row">
           {/* column1 */}
           <div className="col">
-            <h4>Team CoAcco</h4>
-            <p>provide Acomodation choice and help booking until we move you in and service you beyond.</p>
+            <h4><b>Team CoAcco</b></h4>
+            <p className="footer-span"><span>provide Acomodation choice and help booking until we move you in and service you beyond.</span></p>
             <ul className = "list-unstyled">
-              <li>Organization Nam: CoAcco</li>
+              <li>Organization Name: CoAcco</li>
               <li>Web Site:</li>
               <li>visit us</li>
               <li>CoAcco's Office</li>
@@ -23,7 +25,7 @@ function App() {
           </div>
           {/* column2 */}
           <div className="col">
-            <h4>COMPANY</h4>
+            <h4><b>Company</b></h4>
             <ul className = "list-unstyled">
               <li><a >About Us</a></li>
               <li><a >Contact Us</a></li>
@@ -33,13 +35,14 @@ function App() {
           </div>
           {/* column3 */}
           <div className="col">
-            <h4>CONTACT US</h4>
+            <h4><b>Contact Us</b></h4>
             <ul className = "list-unstyled">
               <li>Toll Free</li>
               <li>Email:</li>
               <li>CONNECT WITH US</li>
               <li>
-                <span className="fa-facebook-square"></span>
+                <FontAwesomeIcon icon="facebook" />
+                {/* <span className="fa-facebook-square"></span> */}
                 {/* <a href="#">
                   <FontAwesomeIcon icon = {faYoutube} size="2x" />
                 </a> */}
@@ -51,8 +54,8 @@ function App() {
         </div>
         <br/>
         <div className = "row">
-          <p className = "col-sm">
-            &copy;{new Date().getFullYear()} coompanyName
+          <p className = "col-sm footer-copyright">
+            &copy;{new Date().getFullYear()} Team CoAcco. All Rights Reserved.
           </p>
         </div>
       </div>
